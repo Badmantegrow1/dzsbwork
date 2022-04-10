c72f7b67905e3665d0c695e36d6737ec101d9e29
Konstantin <Badmantegrow1@gmail.com>
2022-04-10 03:16:59 +0300

    решение

--- 07_wall.py
+++ 07_wall.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
+
+
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
+#размеры кирпича он же шаг отступа
 
-simple_draw.pause()
+for y in range(0, 600, 50):
+    x = int(y / 50) % 2 * 50
+    for x in range(x, 600, 100):
+        sd.rectangle(sd.get_point(x, y), sd.get_point(x + 100, y + 50), width=2)
+
+sd.pause()
